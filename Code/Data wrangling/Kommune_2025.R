cb43790ee6ef9afe0ce9835203630bf6daf77462
sanderseriksen <133579340+sanderseriksen@users.noreply.github.com>
2025-03-19 12:25:33 +0100

    Added kommune data with demographic and area

--- Code/Data wrangling/Kommune_2025.R
+++ Code/Data wrangling/Kommune_2025.R
@@ -3,11 +3,12 @@
 # Importing libraries
 library(tidyverse)
 library(readxl)
+library(writexl)
 
 # Set locale to UTF-8
 Sys.setlocale("LC_ALL", "en_US.UTF-8")
 
-# Read data
+# Read data for total population and area of each municipality
 kommune_data <- read_excel("Kommune_data.xlsx", skip = 3) %>% 
   rename("Municipality" = "...1",
          "Population" = "2025...2",
@@ -18,3 +19,23 @@ kommune_data <- read_excel("Kommune_data.xlsx", skip = 3) %>%
   mutate(Population = as.numeric(Population),
          Area = as.numeric(Area))
 
+# Read data for demographic data
+demographic_data <- read_excel("Kommune_demo.xlsx", skip = 4) %>% 
+  rename("Municipality" = "...1",
+         "0-17" = "0-17 år",
+         "18+" = "18 år eller eldre") %>%
+  filter(if_all(everything(), ~ !is.na(.) & . != 0)) %>%  # Remove rows with NA or 0 in any column
+  separate(Municipality, into = c("Mun_num", "Mun_name"), sep = " ") %>%
+  separate(Mun_num, into = c("K", "Mun_num"), sep = "-") %>% 
+  select(-"K")
+
+# Merge the two datasets
+kommune_data_final <- kommune_data %>% 
+  left_join(demographic_data, by = c("Mun_num", "Mun_name"))
+
+# Write data to Excel
+write_xlsx(kommune_data_final, "Kommune_data_final.xlsx")
+
+
+
+  
