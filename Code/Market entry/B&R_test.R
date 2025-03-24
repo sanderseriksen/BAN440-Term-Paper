2bbfe3dd99a9c7050e72a85b45f11d65aba49300
sanderseriksen <133579340+sanderseriksen@users.noreply.github.com>
2025-03-24 12:48:09 +0100

    Updated B&R model

--- Code/Market entry/B&R_test.R
+++ Code/Market entry/B&R_test.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 library(readxl)
 library(fastDummies)
-library(MASS)
+library(knitr)
 
 # Set locale to UTF-8
 Sys.setlocale("LC_ALL", "en_US.UTF-8")
@@ -87,15 +87,88 @@ br_data <- Vinmonopolet_market %>%
 table(br_data$Number_of_stores)
 
 # Adding variables to the data
+upperb <- 2
+
 br_data <- br_data %>% 
   mutate(
-    as.factor(Number_of_stores) = Number_of_stores,
-    s = Population / 1000
+    s = Population / 1000,
+    density = Number_of_stores / Area,
+    Number_of_stores = as.factor(ifelse(Number_of_stores <= upperb, Number_of_stores, upperb))
   ) %>% 
-  dummy_cols(select_columns = "Number_of_stores")
+  dummy_cols(select_columns = "Number_of_stores") %>% 
+  mutate_at(vars(starts_with("Number_of_stores")), as.factor)
 
 str(br_data)
 
+# Fitting the Bresnahan & Reiss model
+library(MASS)
+
 model_1 <- polr(Number_of_stores ~ s, data = br_data, method = "probit")
 
-summary(model1)
+summary(model_1)
+
+
+model_2 <- polr(Number_of_stores ~ s + density, data = br_data, method = "probit")
+
+summary(model_2)
+
+
+## Model 1 ##
+
+# Extract coefficients and cutoffs
+lambda1 <- model_1$coefficients  # Estimate for s
+theta1 <- model_1$zeta  # Cutoff points
+
+# Compute S_N (Thresholds for s)
+S_N1 <- exp(theta1)  # Since there is only one predictor, no need for mean adjustment
+
+# Create labels for S_N
+upperb1 <- length(theta1)  # Number of thresholds
+slab1 <- paste0("$S_", 1:upperb1, "$")
+names(S_N1) <- slab1
+
+# Compute ETR_N using the cutoffs
+ETR_N1 <- exp(theta1[2:upperb1] - theta1[1:(upperb1-1)]) * (1:(upperb1-1)) / (2:upperb1)
+
+# Create labels for ETR_N
+elab1 <- paste0("$s_", 2:upperb1, "/s_", 1:(upperb1-1), "$")
+names(ETR_N1) <- elab1
+
+# Print results
+S_N1
+ETR_N1
+
+kable(S_N1, col.names = c("'000s"), digits = 4,
+      caption = 'Entry thresholds',
+      booktabs = TRUE)
+
+## Model 2 ##
+
+# Extract coefficients and cutoffs
+lambda <- model_2$coefficients  # Estimates for s and density
+theta <- model_2$zeta  # Cutoffs
+
+# Compute S_N using the new predictors
+S_N <- exp(theta - mean(br_data$density) * lambda["density"])
+
+# Create labels for S_N
+upperb <- length(theta)  # Number of thresholds
+slab <- paste0("$S_", 1:upperb, "$")
+names(S_N) <- slab
+
+# Compute ETR_N using the cutoffs
+ETR_N <- exp(theta[2:upperb] - theta[1:(upperb-1)]) * (1:(upperb-1)) / (2:upperb)
+
+# Create labels for ETR_N
+elab <- paste0("$s_", 2:upperb, "/s_", 1:(upperb-1), "$")
+names(ETR_N) <- elab
+
+# Print results
+S_N
+ETR_N
+
+kable(S_N, col.names = c("'000s"), digits = 4,
+             caption = 'Entry thresholds',
+             booktabs = TRUE)
+
+table(br_data$Number_of_stores)
